4129fdc70f59d9ce8265d5b500e65dbb20cdf8ab
TanmayVedpathak <tanmayvedpathak19@gmail.com>
2025-09-23 23:04:45 +0530

    comment routes completed

--- controllers/commentControllers.js
+++ controllers/commentControllers.js
@@ -44,4 +44,88 @@ const addComment = asyncHandler(async (req, res) => {
   res.redirect(`/posts/${postId}`);
 });
 
-module.exports = { addComment };
+// get comment form
+const getCommentForm = asyncHandler(async (req, res) => {
+  const comment = await Comment.findById(req.params.id);
+
+  if (!comment) {
+    return res.render("postDetails", {
+      title: "Post",
+      post: "",
+      user: req.user,
+      error: "Post not found",
+      success: "",
+    });
+  }
+
+  res.render("editComment", {
+    title: "Comment",
+    comment,
+    user: req.user,
+    error: "",
+    success: "",
+  });
+});
+
+// update comment
+const updateComment = asyncHandler(async (req, res) => {
+  const { content } = req.body;
+  const comment = await Comment.findById(req.params.id);
+
+  if (!comment) {
+    return res.render("postDetails", {
+      title: "Post",
+      post: "",
+      user: req.user,
+      error: "Comment not found",
+      success: "",
+    });
+  }
+
+  if (comment.author._id.toString() !== req.user.id.toString()) {
+    return res.render("postDetails", {
+      title: "Post",
+      post: "",
+      user: req.user,
+      error: "You are not authorized to edit this comment",
+      success: "",
+    });
+  }
+
+  comment.content = content || comment.content;
+
+  await comment.save();
+
+  res.redirect(`/posts/${comment.post}`);
+});
+
+// delete comment
+const deleteComment = asyncHandler(async (req, res) => {
+  const comment = await Comment.findById(req.params.id);
+
+  if (!comment) {
+    return res.render("postDetails", {
+      title: "Post",
+      post: "",
+      user: req.user,
+      error: "Comment not found",
+      success: "",
+    });
+  }
+
+  if (comment.author._id.toString() !== req.user.id.toString()) {
+    return res.render("postDetails", {
+      title: "Post",
+      post: "",
+      user: req.user,
+      error: "You are not authorized to edit this comment",
+      success: "",
+    });
+  }
+
+  await Comment.findByIdAndDelete(req.params.id);
+
+  res.redirect(`/posts/${comment.post}`);
+});
+
+module.exports = { addComment, getCommentForm, updateComment, deleteComment };
--- routes/commentRoutes.js
+++ routes/commentRoutes.js
@@ -1,9 +1,24 @@
 const express = require("express");
 const { ensureAuthenticate } = require("../middlewares/auth");
-const { addComment } = require("../controllers/commentControllers");
+const {
+  addComment,
+  getCommentForm,
+  updateComment,
+  deleteComment,
+} = require("../controllers/commentControllers");
 
 const commentRoutes = express.Router();
 
+// add comment
 commentRoutes.post("/posts/:id/comments", ensureAuthenticate, addComment);
+
+// get comment form
+commentRoutes.get("/comments/:id/edit", getCommentForm);
+
+// update comment
+commentRoutes.put("/comments/:id", ensureAuthenticate, updateComment);
+
+// delete comment
+commentRoutes.delete("/comments/:id", ensureAuthenticate, deleteComment);
 
 module.exports = commentRoutes;
